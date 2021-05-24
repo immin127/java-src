7ae681f15986bdc20ed1bfc83bfc47637804491b
immin127 <immin127@gmail.com>
2021-05-24 22:35:37 +0900

    commit

---
+++ Basic/Test02.java
@@ -0,0 +1,30 @@
+package Basic;
+
+public class Test02 {
+    public static void main(String[] args) {
+        // 논리연산자
+        // (||)대문자와 소문자를 다른문자로 인식(->같은문자로 인식하게)
+        char logic1 = 'a';
+        boolean result = logic1=='A'||logic1=='a'; //->A와 a가 같니? 라고 물어봄
+        System.out.println(result); // ||를 써서 A와 a를 자바가 같은 문자로 인식하게됨, ||은 or와 같음
+        
+        //&&숫자의 범위를 표현할때 사용
+        int number=11;
+        String result1=(number>0 && number<=10)?"1부터 10사이의 숫자":"1부터 10사이의 숫자가 아님";
+        System.out.println(result1);
+        
+        //! 부정, 반대로 사용 (전원버튼에 많이 사용)
+        boolean power = false;
+        System.out.println(!power); //->true
+        power = !power;
+        System.out.println(!power); //->false
+        
+        String result2 = (!power==true)?"전원이 작동함":"전원이 꺼졌다.";
+        System.out.println(result2);
+        power=true;
+        result2 = (!power==true)?"전원이 작동함":"전원이 꺼졌다.";
+        System.out.println(result2);
+    }
+
+
+}
---
+++ Basic/Test03.java
@@ -0,0 +1,31 @@
+package Basic;
+
+import java.util.ArrayList;
+
+public class Test03 {
+    public static void main(String[] args) {
+        // 형변환
+        // 기본형 변환
+        // 큰 범위의 형으로 변환
+        int a = 10;
+
+        long b = a;
+        System.out.println(a);
+        
+        //작은 형태로 변환
+        long c = 10;
+        int d = (int) c;
+        System.out.println(d);
+
+        double e = 1.5;
+        int f = (int) e; //소수점이 버려져 1이됨.
+        System.out.println(f);
+
+        // long g = 2147483648;
+        // int h = (int) g; // 오버플로우로 - 2147483648
+        // System.out.println(h);
+    
+
+
+    }
+}
